0ee0a8ae056f908f2b8c79cfb0795633ae4926fc
soufianemj1 <elmajati.s@gmail.com>
2021-11-24 16:43:10 +0100

    final project

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 let student = new Array();
 document.getElementById('tables').style.display = 'none';
-let now = moment();
-
-
-
 
 function save() {
     document.getElementById('tables').style.display = 'flex';
@@ -18,6 +14,7 @@ function save() {
     document.getElementById('name').value = '';
     document.getElementById('subject').value = '';
     display();
+    // console.log(student);
 
 }
 
@@ -35,46 +32,65 @@ function display() {
 
 }
 
+var arrRst = new Array();
 function random() {
 
+    var step1 = student.length ; 
+    var step2 = Math.random() * step1; 
+    var result = Math.floor(step2); 
+
+    
+    var StudentNumber = student[result];
+    student.splice(result, 1);
+    arrRst.push({
+        name: StudentNumber.studentname,
+        subject: StudentNumber.subject,
+    });
+    console.log(arrRst)
+
+    resutlatRandom();
+;}
+
+function resutlatRandom(){
     var table2 = document.getElementById('table2');
-    var row2 = table2.insertRow();
-    var cell21 = row2.insertCell(0);
-    var cell22 = row2.insertCell(1);
-    var cell23 = row2.insertCell(2);
-    var max = student.length;
-    var i = 0;
-    var randStudent = new Array();
-    var projectday = new Array();
-    var skipe = moment(projectday);
-    
-    
-    
-    i = Math.floor(Math.random() * (max));
-    randStudent.push(student[i]);
-    student.splice(i, 1);
-
-
-   
-    
-
-    // if(skipe == 4){
-    //     projectday.push(now.add(3, 'days').format('DD-MM-YYYY'));
-    // }else if(skipe == 5){
-    //     projectday.push(now.add(2, 'days').format('DD-MM-YYYY'));
-    // }else{
-    //     projectday.push(now.add(1, 'days').format('DD-MM-YYYY'));
-    // }
-    // console.log(projectday)
-
-    projectday.push(now.add(1, 'days').format('DD-MM-YYYY'));
-    
-    for (let f = 0; f < randStudent.length; f++) {
-        cell21.innerHTML = randStudent[f].studentname;
-        cell22.innerHTML = randStudent[f].subject;
-        cell23.innerHTML = projectday[f];
-        
+    var x = table2.rows.length;
+    //delete day dupplicate
+    while(--x){
+        table2.deleteRow(x);
     }
+
+    var date = new Date();
+    for(var i=0 ; i < arrRst.length ; i++){
+        //day auto increment
+        if(date.getDay()+1 == 6){
+            date.setDate(date.getDate ()+ 3)
+        }else {
+            date.setDate(date.getDate()+ 1)
+        }
+
+        var row2 = table2.insertRow();
+        var cell1 = row2.insertCell();
+        var cell2 = row2.insertCell();
+        var cell3 = row2.insertCell();
+  
+        cell1.innerHTML = arrRst[i].name;
+        cell2.innerHTML = arrRst[i].subject;
+        cell3.innerHTML = date.toLocaleDateString();
+    }
+}
+
+
+//exportation function
+function Exportation(type, fn, dl) {
+    var elt = document.getElementById('table2');
+    var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
+    return dl ?
+        XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }) :
+        XLSX.writeFile(wb, fn || ('final result.' + (type || 'xlsx')));
+}
+
+
+    
+    
    
 
-}
